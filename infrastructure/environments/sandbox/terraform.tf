c400dff2e3c202259d71582106cba778635c8ccf
marcelooliveira <mclricardo@gmail.com>
2025-03-30 10:45:24 -0300

    create s3 bucket

--- infrastructure/environments/sandbox/terraform.tf
+++ infrastructure/environments/sandbox/terraform.tf
@@ -20,6 +20,30 @@ provider "aws" {
   secret_key = var.terraform_aws_secret_key
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3-function-bucket-2"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 #Call module lambda Terraform
 module "lambda-functions" {
   source                   = "../../modules/lambda-functions"
