598fa7ea874179e2e4eadbb5305344d999bf1142
marcelooliveira <mclricardo@gmail.com>
2025-03-30 10:49:34 -0300

    removed bucket

--- infrastructure/environments/sandbox/terraform.tf
+++ infrastructure/environments/sandbox/terraform.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
- backend "s3" {
-   encrypt = true    
-   bucket   = "s3-function-bucket-2"
-   key      = "s3-function-folder/terraform.tfstate"
-   region   = "us-east-1"
- }
+# backend "s3" {
+#   encrypt = true    
+#   bucket   = "s3-function-bucket-2"
+#   key      = "s3-function-folder/terraform.tfstate"
+#   region   = "us-east-1"
+# }
 }
 
 # Creating AWS Provider
@@ -18,30 +18,6 @@ provider "aws" {
   region = "us-east-1"
   access_key = var.terraform_aws_access_key
   secret_key = var.terraform_aws_secret_key
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "s3-function-bucket-2"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
 }
 
 #Call module lambda Terraform
